a9e26df1499a508afbe0bdcfdb994d2a35a5ec72
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2022-10-07 20:16:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
+const[count,setCount]=useState(1)
 
+    const IncrementHandler=()=>{
+          setCount(count+1)
+    }
+
+    const DecrementHandler=()=>{
+        if(count != 1){
+            setCount(count-1)
+        }
+
+    }
   
   return (
     <div id="main">
+     <div id='count'>
+        <h2 className= {count % 5 == 0 && count % 3 == 0 ? 'fizzbuzz' : (count % 3 == 0) ? 'fizz' : (count % 5 == 0 ) ? 'buzz' : 'normal'}>{count}</h2>
+        <button id='increment' onClick={IncrementHandler}>Increment</button>
+        <button id='decrement' onClick={DecrementHandler}>Decrement</button>
+    </div>
 
     </div>
   )
